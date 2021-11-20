b381dff2e3f98be4bf481dfa8bc354a127b84680
Louie <liu.lo@husky.neu.edu>
2021-11-19 20:11:57 -0500

    Threaded Client and Server complete, with 25ms delay gives around 10 concurrent threads.

---
+++ ThreadedClient.cpp
@@ -0,0 +1,97 @@
+#include <WS2tcpip.h>
+
+#include <exception>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
+#pragma comment (lib, "ws2_32.lib")
+
+// used to lock and unlock std::cout for pretty-printing to console/file
+std::mutex mu;
+
+void createConnection(int id) {
+	const std::string IP_ADDRESS = "127.0.0.1";
+	const int PORT = 54000;
+	const std::string FILENAME = "RandomText.txt";
+
+	// Create tcp socket
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		WSACleanup();
+		throw std::runtime_error("Can't create socket");
+	}
+
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(PORT);
+	inet_pton(AF_INET, IP_ADDRESS.c_str(), &hint.sin_addr);
+
+	// create the connection to the server
+	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR)
+	{
+		closesocket(sock);
+		WSACleanup();
+		throw std::runtime_error("Can't connect to server");
+	}
+	
+	mu.lock();
+	std::cout << "Thread with id: " << id << " connected to server!" << std::endl;
+	mu.unlock();
+
+	const int BUFSIZE = 4096;
+	char buf[BUFSIZE];
+	ZeroMemory(buf, BUFSIZE);
+
+	// Receive message from server asking to provide a filename
+	int byteCount = recv(sock, buf, BUFSIZE, 0);
+	if (byteCount == SOCKET_ERROR) {
+		throw std::runtime_error("Error receiving message");
+	}
+
+	if (byteCount == 0) {
+		throw std::runtime_error("Server disconnected");
+	}
+
+	// send the name of the known file
+	strcpy_s(buf, FILENAME.c_str());
+	byteCount = strlen(FILENAME.c_str());
+	send(sock, buf, byteCount, 0);
+
+	// receive the message
+	byteCount = recv(sock, buf, BUFSIZE, 0);
+	mu.lock();
+	std::cout << "Thread with id: " << id << " received: " << std::string(buf) << std::endl;
+	mu.unlock();
+}
+
+void main() {
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+	const int NUM_THREADS = 100;
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0) {
+		std::cerr << "can't initialize winsock! quitting!" << std::endl;
+		return;
+	}
+
+	// create a vector of threads
+	std::vector<std::unique_ptr<std::thread>> threads;
+	for (int i = 1; i <= NUM_THREADS; i++) {
+		// spawn a new thread and create a new connection
+		threads.emplace_back(new std::thread(createConnection, i));
+	}
+
+	// join all the threads after spawning
+	for (int i = 0; i < NUM_THREADS; i++) {
+		threads.at(i)->join();
+		mu.lock();
+		std::cout << "Destroying thread with id: " << i+1 << std::endl;
+		mu.unlock();
+	}
+}
---
+++ ThreadedServer.cpp
@@ -0,0 +1,165 @@
+#include <WS2tcpip.h>
+
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <string>
+#include <sstream>
+
+#pragma comment (lib, "ws2_32.lib")
+
+// Used to lock std::cout for pretty printing to console
+// and to increment/decrement concurrentThreads
+std::mutex mu;
+static int concurrentThreads = 0;
+
+void handleConnection(SOCKET clientSocket, int i) {
+	mu.lock();
+	concurrentThreads++;
+	std::cout << "Thread " << i << "connected, concurrentThreads: " << concurrentThreads << std::endl;
+	mu.unlock();
+
+	const int BUFSIZE = 4096;
+	char buf[BUFSIZE];
+	ZeroMemory(buf, BUFSIZE);
+
+	// Receive a message from the client
+	int byteCount = recv(clientSocket, buf, BUFSIZE, 0);
+
+	mu.lock();
+	std::cout << "Thread " << i << " received message" << std::endl;
+	mu.unlock();
+
+	if (byteCount == SOCKET_ERROR) {
+		std::cerr << "Error in recv(), quitting" << std::endl;
+		return;
+	}
+
+	if (byteCount == 0) {
+		std::cout << "Client disconnected " << std::endl;
+		return;
+	}
+
+	// Determine if the requested file exists
+	std::ifstream f(buf);
+	if (!f.good()) {
+		std::string text = "File doesn't exist!";
+		strcpy_s(buf, text.c_str());
+		byteCount = strlen(text.c_str());
+		// tell the client the file doesn't exist and close the socket
+		send(clientSocket, buf, byteCount, 0);
+		closesocket(clientSocket);
+		return;
+	}
+
+	// write the contents of the file to stringstream, and then a string
+	std::ostringstream ss_buf;
+	ss_buf << f.rdbuf();
+	std::string outText = ss_buf.str();
+
+	// Send the contents of the file back to the client
+	strcpy_s(buf, outText.c_str());
+	byteCount = strlen(outText.c_str());
+
+	mu.lock();
+	std::cout << "Thread " << i << "sending message" << std::endl;
+	mu.unlock();
+
+	// A pause just so we can get more concurrent threads, this operation
+	// is so quick that usually there aren't more than 1 or 2 concurrent threads.
+	// Sleeping 25ms usually gives around 10 concurrent threads at a time
+	Sleep(25);
+	send(clientSocket, buf, byteCount, 0);
+	closesocket(clientSocket);
+	f.close();
+
+	mu.lock();
+	concurrentThreads--;
+	std::cout << "Thread " << i << "Closing Connection! concurrentThreads: " << concurrentThreads << std::endl;
+	mu.unlock();
+}
+
+void main() {
+	// initialize winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0) {
+		std::cerr << "can't initialize winsock! quitting!" << std::endl;
+		return;
+	}
+
+	// create a socket
+	// SOCK_STREAM is TCP socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET) {
+		std::cerr << "Can't create a socket, quitting" << std::endl;
+		WSACleanup();
+		return;
+	}
+
+	// bind the ip address and port to the socket
+	std::string ipAddress = "127.0.0.1";
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	// host to network short to go between big and little endian
+	hint.sin_port = htons(54000);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+	// tell winsock the socket is for listening
+	listen(listening, SOMAXCONN);
+	std::cout << "Listening for connections..." << std::endl;
+
+	// Keep track of the thread count for concurrency
+	int threads = 1;
+	while (true) {
+		// wait for connection
+		sockaddr_in client;
+		int clientSize = sizeof(client);
+
+		// accept a new client
+		SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+		if (clientSocket == INVALID_SOCKET) {
+			std::cerr << "can't create client socket, quitting" << std::endl;
+			WSACleanup();
+			return;
+		}
+
+		char host[NI_MAXHOST];    // Client's remote name
+		char service[NI_MAXHOST]; // Service (i.e. port) the client is connected on
+
+		ZeroMemory(host, NI_MAXHOST);
+		ZeroMemory(service, NI_MAXHOST);
+
+		if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
+			mu.lock();
+			std::cout << host << " connected on port " << service << std::endl;
+			mu.unlock();
+		}
+		else
+		{
+			inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+			mu.lock();
+			std::cout << host << " connected on port " << ntohs(client.sin_port) << std::endl;
+			mu.unlock();
+		}
+
+		// tell the user to request a file
+		std::string reqString = "Please request a file: ";
+		send(clientSocket, reqString.c_str(), strlen(reqString.c_str()), 0);
+		// create the thread for the current client --> server connection
+		std::thread th(handleConnection, clientSocket, threads);
+		// detach it to keep it running as it leaves scope
+		th.detach();
+		threads++;
+	}
+
+	// close listening socket
+	closesocket(listening);
+	// Cleanup winsock
+	WSACleanup();
+}
